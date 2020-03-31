ae2fcfb9c013d831e6c6c96a5424eeb353801c37
William Trout <troutw5593@gmail.com>
2020-03-31 18:08:55 -0500

    adding js page and photos

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-// javascript
+var questions = 
+[
+    {
+        question: "What is the question?",
+        choiceA: "answer",
+        choiceB: "answer",
+        choiceC: "answer",
+        choiceD: "answer",
+        correct: "C",
+    },
+
+     {
+        question: "What is the question?",
+        choiceA: "answer",
+        choiceB: "answer",
+        choiceC: "answer",
+        choiceD: "answer",
+        correct: "C",
+    },
+
+        {
+        question: "What is the question?",
+        choiceA: "answer",
+        choiceB: "answer",
+        choiceC: "answer",
+        choiceD: "answer",
+        correct: "C",
+    },
+
+        {
+        question: "What is the question?",
+        choiceA: "answer",
+        choiceB: "answer",
+        choiceC: "answer",
+        choiceD: "answer",
+        correct: "C",
+    },
+
+    {
+        question: "What is the question?",
+        choiceA: "answer",
+        choiceB: "answer",
+        choiceC: "answer",
+        choiceD: "answer",
+        correct: "c",
+    }
+]
